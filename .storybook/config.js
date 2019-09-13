76b3c7c2ec02f12fa261653da63122b742d60b66
William Velazquez <william.velazquez.a@gmail.com>
2019-09-13 18:57:31 -0500

    Theming Configuration, Adding Pink Panther Theme, Adding William Logo and Icon and Updating Port

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,26 @@
 //@Author William E. Velázquez A. - info@williamvelazquez.com
 import { configure } from '@storybook/react';
 
+import { addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
+
+// import pinkPanther from  './pinkPanther';
+import williamLogo from '../src/images/LogoWhite_55.png';
+
+addParameters({
+  options: {
+    theme: {
+      ...themes.dark,
+      colorSecondary: '#8e1a1a',
+      brandTitle: `William's Personal Design System`,
+      brandUrl: 'https://williamvelazquez.com/',
+      brandImage: williamLogo
+    }
+    // theme: themes.dark
+    // theme: pinkPanther
+  }
+});
+
 //We use context from Webpack to change the context to search the stories.
 //Path where we search, Search on subdirectories?, Regular Expression to search files.
 const req = require.context('../src/components', true, /\.stories\.js$/);
